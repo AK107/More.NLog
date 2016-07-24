240b2300dbb82e283c197ee42ac59e35c0501a3a
AK107 <adm.Beat@gmail.com>
2016-07-24 22:09:21 +0300

    nuspec edited and moved to project directory.

--- More.NLog/Properties/AssemblyInfo.cs
+++ More.NLog/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("More.NLog")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("More.NLog")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("AK107")]
 [assembly: AssemblyProduct("More.NLog")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
